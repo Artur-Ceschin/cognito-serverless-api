eec6d993f7d47626d5f645987882844b12452e20
Artur Ceschin <artur.ceschin@gmail.com>
2025-10-30 16:14:01 -0300

    feat: sign and confirmation

---
+++ src/functions/signIn.ts
@@ -0,0 +1,37 @@
+import { cognitoClient } from '@/libs/cognitoClient';
+import { bodyParser } from '@/utils/bodyParser';
+import { response } from '@/utils/response';
+import { InitiateAuthCommand, NotAuthorizedException, UserNotConfirmedException, UserNotFoundException } from '@aws-sdk/client-cognito-identity-provider';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const { email, password } = bodyParser(event.body);
+
+    const command = new InitiateAuthCommand({
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+      },
+    })
+
+    const { AuthenticationResult } = await cognitoClient.send(command);
+    if(!AuthenticationResult) {
+      return response(400, { message: 'Invalid credentials' });
+    }
+
+    const { AccessToken, RefreshToken } = AuthenticationResult;
+
+    return response(200, { accessToken: AccessToken, refreshToken: RefreshToken });
+  } catch (error) {
+    if(error instanceof UserNotFoundException)
+      return response(401, { message: 'Invalid credentials' });
+    if(error instanceof NotAuthorizedException)
+      return response(401, { message: 'Invalid credentials' });
+    if(error instanceof UserNotConfirmedException)
+      return response(401, { message: 'Confirm your account before signing in' });
+    return response(500, { message: 'Internal server error' });
+  }
+}
